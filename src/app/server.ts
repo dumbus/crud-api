2b70aa380fcf397aabfae1f6e456b2a8f3639c1f
dumbus <dumbusx@gmail.com>
2023-01-16 16:24:27 +0300

    feat: add checkMultiMode module

--- src/app/server.ts
+++ src/app/server.ts
@@ -1,14 +1,26 @@
 import * as dotenv from 'dotenv';
 import { createServer } from 'node:http';
 import { requestListener } from './requestListener';
+import { checkMultiMode } from './utils/checkMultiMode';
 
 dotenv.config();
 
 const port = process.env.PORT || 3000;
-export const server = createServer(requestListener);
+const server = createServer(requestListener);
 
-const startServer = () => {
-    server.listen(port, () => console.log(`Server started on port ${port}`));
+const isMultiMode = checkMultiMode();
+
+let startServer;
+
+if (isMultiMode) {
+    // balancer
+    startServer = () => console.log('balancer');
+} else {
+    startServer = () => {
+        server.listen(port, () => console.log(`Server started on port ${port}`));
+    }
 }
 
-export { startServer };
+ 
+
+export { startServer, server };
