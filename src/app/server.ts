11120167458fbb950ab29667988b546eb6399fa1
dumbus <dumbusx@gmail.com>
2023-01-16 16:28:46 +0300

    refactor: change startServer structure

--- src/app/server.ts
+++ src/app/server.ts
@@ -5,22 +5,18 @@ import { checkMultiMode } from './utils/checkMultiMode';
 
 dotenv.config();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 const server = createServer(requestListener);
 
 const isMultiMode = checkMultiMode();
 
-let startServer;
-
-if (isMultiMode) {
-    // balancer
-    startServer = () => console.log('balancer');
-} else {
-    startServer = () => {
+const startServer = () => {
+    if (isMultiMode) {
+        // balancer
+        console.log('balancer');
+    } else {
         server.listen(port, () => console.log(`Server started on port ${port}`));
     }
-}
-
- 
+} 
 
 export { startServer, server };
